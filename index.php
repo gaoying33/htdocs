b98ddc769d9434bee11567286f1769fda64effb6
gaoying33 <amygao92@163.com>
2015-06-03 10:53:26 +0800

    补传

---
+++ index.php
@@ -0,0 +1,15 @@
+﻿<?php
+	//开启调试模式
+	define('APP_DEBUG', true);
+	
+	if ($_SERVER['REQUEST_METHOD']=='GET')
+	{
+		define('APP_NAME', 'AboutUs');
+		define('APP_PATH', './AboutUs/');
+	}
+	elseif ($_SERVER['REQUEST_METHOD']=='POST')
+	{
+		define('APP_NAME', 'AboutUsOperate');
+		define('APP_PATH', './AboutUsOperate/');
+	}
+	require './ThinkPHP/ThinkPHP.php';
